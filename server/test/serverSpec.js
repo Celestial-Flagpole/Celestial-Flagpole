20687c3d3bd0d19f00dd018d94fb3899095b3a7d
Andres <andres.mm07@gmail.com>
2015-11-12 11:58:40 -0800

    changes to test

--- server/test/serverSpec.js
+++ server/test/serverSpec.js
@@ -1,21 +1,53 @@
+var expect = require('chai').expect;
 var request = require('supertest');
+//nock is an HTTP mocking
+var nock = require('nock');
+
 describe('loading express', function() {
   var server;
   //start and stop the server to make tests order independent
-  beforeEach(function (){
-    server = require('../app', {bustCache: true});
+  beforeEach(function (done){
+    server = require('./server', {bustCache: true});
+    //make sure app is listening
+    server.listen(8686, function(err, result){
+      if(err){
+        done(err);
+      }else{
+        done();
+      }
+    });
   });
+
   afterEach(function (done) {
     server.close(done);
   });
-  it('responds to /', function defaultRoute (done) {
-    request(server)
-      .get('/')
-      .expect(200, done);
+
+  //requests to server
+  describe('basic requests', function () {
+    //test to check if our server responds to this request
+    it('responds to /', function defaultRoute (done) {
+      request(server)
+        .get('/')
+        .expect(200, done);
+    });
+    //should throw a 404 when users make a request to unknown route
+    it('404 route not defined', function wrongRoute (done) {
+      request(server)
+        .get('/wrong/route')
+        .expect(404, done);
+    });
   });
-  it('404 route not defined', function wrongRoute (done) {
-    request(server)
-      .get('/wrong/route')
-      .expect(404, done);
+
+  //request to third party 
+  describe('youtube get', function() {
+   var youtube = nock('youtubeURL')
+    .get('/video/endpoint')
+    .reply(200, {
+      data: {
+        id: "videoid",
+        name: "videoName",
+      }
+    });
   });
+
 });
